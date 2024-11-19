1fcfa44d06e21a3fa25c04dfbca990cbfd48372e
Debmalya <debmalya.mondal@outlook.com>
2024-11-19 10:24:07 +0530

    sql solution

---
+++ SQL/SQL Server/Medium/immediate_food_delivery2_prob_1174.sql
@@ -0,0 +1,18 @@
+with deliv_extn as
+(select customer_id, order_date,
+case
+	when DATEDIFF(DAY, order_date, customer_pref_delivery_date) = 0 then 'immediate'
+	else 'scheduled'
+	end as ordtype,
+FIRST_VALUE(order_date) over(partition by customer_id order by order_date) as first_order,
+count(customer_id) over(partition by customer_id) as totalorders
+from delivery),
+tblinter as
+(select customer_id, count(case when ordtype = 'immediate' then customer_id end) as immdcount
+from deliv_extn
+where order_date = first_order
+group by customer_id
+)
+
+select round(((sum(immdcount) * 1.00 / count(*)) * 100), 2) as immediate_percentage
+from tblinter
